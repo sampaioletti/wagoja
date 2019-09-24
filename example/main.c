4c681980bea2e37b1f53ee50e40878613c8e1c72
sampaioletti <sampaioletti@gmail.com>
2019-09-23 19:16:28 -0600

    Fixed compile with comment

--- example/main.c
+++ example/main.c
@@ -1,4 +1,4 @@
-//compile with emcc mult.c -Os -s WASM=1 -s SIDE_MODULE=1 -o mult.wasm -s EXPORTED_FUNCTIONS="['_mult']"
+//compile with emcc main.c EXPORTED_FUNCTIONS="['_mult','main']"
 #include <stdio.h>
 int hello(int x) {
   puts("Hello From WaSM");
